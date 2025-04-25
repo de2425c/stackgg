0c0a32ffcb8b768e8f3cf2035cd55b944d288218
David Eyal <de2425@columbia.edu>
2025-04-25 15:46:21 -0400

    Initial Commit

---
+++ stack/stackApp.swift
@@ -0,0 +1,17 @@
+//
+//  stackApp.swift
+//  stack
+//
+//  Created by David Eyal on 4/25/25.
+//
+
+import SwiftUI
+
+@main
+struct stackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ stackTests/stackTests.swift
@@ -0,0 +1,17 @@
+//
+//  stackTests.swift
+//  stackTests
+//
+//  Created by David Eyal on 4/25/25.
+//
+
+import Testing
+@testable import stack
+
+struct stackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
